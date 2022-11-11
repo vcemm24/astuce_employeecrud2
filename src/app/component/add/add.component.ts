fbbfbbb880057645955689f973af5a314bcbdece
vcemm24 <vcemm24@gmail.com>
2022-11-11 06:57:07 +0100

    step2 form for adding completed

--- src/app/component/add/add.component.ts
+++ src/app/component/add/add.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+interface gender{
+  value:string;
+  viewvalue:string;
+}
+interface position{
+  value:string,
+  viewvalue:string
+}
 
 @Component({
   selector: 'app-add',
@@ -6,7 +14,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+usergender:gender[]=[
+ { value:'male',viewvalue:'male'},
+  {value:'female',viewvalue:'female'}
+]
+userposition:position[]=[
+  {value:'angulardev',viewvalue:'Anguler Developer'},
+  {value:'vuedev',viewvalue:'Vue Developer'},
+  {value:'nodedev',viewvalue:'Node Developer'},
+  {value:'laraveldev',viewvalue:'Laravel Developer'},
 
+]
   constructor() { }
 
   ngOnInit(): void {
